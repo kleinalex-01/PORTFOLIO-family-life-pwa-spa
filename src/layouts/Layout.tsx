a8d6a30521173eea249bf75511165683985a6033
kleinalex-01 <kleinalex215@gmail.com>
2025-06-04 19:16:25 +0200

    Quick fix for netfliy test deployment

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 const Layout: React.FC = () => {
     const { user } = useAuth();
