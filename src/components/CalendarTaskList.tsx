338568179aff235440e533baff386830433f45a8
kleinalex-01 <kleinalex215@gmail.com>
2025-06-13 15:50:05 +0200

    Fix for deployment

--- src/components/CalendarTaskList.tsx
+++ src/components/CalendarTaskList.tsx
@@ -2,7 +2,10 @@ import React, { useEffect } from 'react';
 import { onValue } from 'firebase/database';
 import { calendarRef } from '../FirebaseConfig';
 
-
+type Task = {
+    date: string;
+    title: string;
+}
 export const CalendarTaskList: React.FC = () => {
 
     useEffect(() => {
@@ -12,7 +15,7 @@ export const CalendarTaskList: React.FC = () => {
                 console.log("No tasks found");
                 return;
             }
-            const tasks = Object.entries(data).map(([, value]) => ({
+            const tasks = Object.entries(data as Record<string, Task>).map(([, value]) => ({
                 date: value.date,
                 title: value.title,
             }));
